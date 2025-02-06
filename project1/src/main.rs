7a243b52387c5243cb914132ef66eed75353c1fe
Kyle Lin <kylejlin.us@gmail.com>
2025-02-05 18:46:01 -0800

    Outline expand_queue

--- project1/src/main.rs
+++ project1/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::VecDeque;
-
 const PUZZLE_SIZE: usize = 3;
 
 const GOAL_STATE: State = State {
@@ -17,6 +15,19 @@ struct Tile(u8);
 struct State {
     board: [[Tile; PUZZLE_SIZE]; PUZZLE_SIZE],
 }
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+struct Node {
+    state: State,
+
+    /// The cost to reach this node starting from the root.
+    /// For the root node, this is `0`.
+    ///
+    /// This is `g(x)` in the A* algorithm.
+    cost_to_reach: Cost,
+}
+
+type Cost = u32;
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 enum Algorithm {
@@ -42,29 +53,84 @@ fn main() {
     search(initial_state, algorithm);
 }
 
-/**
- * Returns `Some(solution)` if a solution was found, or `None` if no solution was found.
- */
+/// Returns `Some(solution)` if a solution was found, or `None` if no solution was found.
 fn search(initial_state: State, algorithm: Algorithm) -> Option<State> {
-    let mut nodes = VecDeque::new();
-    nodes.push_back(initial_state);
+    // We'll use a simple (albeit inefficient) queue design:
+    // Store the nodes in a vector in descending order of cost.
+    // - To dequeue, just `pop()` the last element.
+    // - To enqueue, insert the new node at the correct position.
+    // This means each enqueue operation is O(n), but the dequeues are O(1),
+    // where n is the length of the queue.
+    //
+    // I'm sure there's a cleaner way to do this with trees or heaps or something,
+    // but that's not the focus of this project, so I'm using this simple approach.
+    let mut queue = Vec::new();
+
+    queue.push(Node {
+        state: initial_state,
+        cost_to_reach: 0,
+    });
 
     loop {
-        let Some(node) = nodes.pop_front() else {
+        let Some(node) = queue.pop() else {
             // If the queue is empty, there is no solution.
             return None;
         };
 
-        if node == GOAL_STATE {
-            return Some(node);
+        if node.state == GOAL_STATE {
+            return Some(node.state);
         }
 
-        node.expand_with_children(&mut nodes, algorithm);
+        expand_queue(&node, &mut queue, algorithm);
     }
 }
 
-impl State {
-    fn expand_with_children(&self, nodes: &mut VecDeque<State>, algorithm: Algorithm) {
+fn expand_queue(parent: &Node, queue: &mut Vec<Node>, algorithm: Algorithm) {
+    parent.for_each_child(|child| {
+        let child_cost = algorithm.cost(&child);
+
+        // Calculate the index where the child should be inserted.
+        let index = queue
+            .iter()
+            // Try to find the first index where the cost is greater than the child's cost...
+            .position(|other| algorithm.cost(other) > child_cost)
+            // ...but if all the costs are less than or equal to the child's cost,
+            // then insert the child at the end.
+            .unwrap_or(queue.len());
+
+        queue.insert(index, child);
+    })
+}
+
+impl Algorithm {
+    /// This is `f(x) := g(x) + h(x)` in the A* algorithm.
+    fn cost(self, node: &Node) -> Cost {
+        node.cost_to_reach + self.estimate_cost_to_goal(node)
+    }
+
+    /// This is `h(x)` in the A* algorithm.
+    fn estimate_cost_to_goal(self, node: &Node) -> Cost {
+        match self {
+            // With Uniform Cost Search, we simply hardcode h(x) to 0.
+            Algorithm::UniformCostSearch => 0,
+
+            Algorithm::MisplacedTileHeuristic => node.number_of_misplaced_tiles(),
+
+            Algorithm::ManhattanDistanceHeuristic => node.manhattan_distance_to_goal(),
+        }
+    }
+}
+
+impl Node {
+    fn for_each_child(&self, mut f: impl FnMut(Node)) {
+        todo!()
+    }
+
+    fn number_of_misplaced_tiles(&self) -> u32 {
+        todo!()
+    }
+
+    fn manhattan_distance_to_goal(&self) -> u32 {
         todo!()
     }
 }

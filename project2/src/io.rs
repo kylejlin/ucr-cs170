9d1ad82bb6b0d03f7eea965379db2330c7685d9a
Kyle Lin <kylejlin.us@gmail.com>
2025-03-15 22:10:13 -0700

    Add forward_search

--- project2/src/io.rs
+++ project2/src/io.rs
@@ -1,10 +1,18 @@
 use super::*;
 
-pub struct BadDatasetSyntaxError {
+pub struct DatasetSyntaxError {
     pub message: String,
 }
 
-impl BadDatasetSyntaxError {
+pub struct Pretty<T>(pub T);
+
+pub trait ToPretty {
+    fn pretty(&self) -> Pretty<&Self> {
+        Pretty(self)
+    }
+}
+
+impl DatasetSyntaxError {
     pub fn new(message: &str) -> Self {
         Self {
             message: message.to_string(),
@@ -12,9 +20,51 @@ impl BadDatasetSyntaxError {
     }
 }
 
-impl std::fmt::Debug for BadDatasetSyntaxError {
+impl std::fmt::Debug for DatasetSyntaxError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "BadDatasetSyntaxError: {}", self.message)
+    }
+}
+
+impl ToPretty for [FeatureStartingFrom1] {}
+
+impl ToPretty for (&[FeatureStartingFrom1], FeatureStartingFrom1) {}
+
+impl std::fmt::Display for Pretty<&[FeatureStartingFrom1]> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Pretty(features) = self;
+
+        write!(f, "{{")?;
+
+        for (i, feature) in features.iter().enumerate() {
+            if i != 0 {
+                write!(f, ",")?;
+            }
+            write!(f, "{}", feature.0)?;
+        }
+
+        write!(f, "}}")
+    }
+}
+
+impl std::fmt::Display for Pretty<&(&[FeatureStartingFrom1], FeatureStartingFrom1)> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Pretty((current_features, candidate_feature)) = self;
+
+        write!(f, "{{")?;
+
+        for (i, feature) in current_features
+            .iter()
+            .chain(std::iter::once(candidate_feature))
+            .enumerate()
+        {
+            if i != 0 {
+                write!(f, ",")?;
+            }
+            write!(f, "{}", feature.0)?;
+        }
+
+        write!(f, "}}")
     }
 }
 
@@ -22,7 +72,7 @@ impl std::fmt::Debug for BadDatasetSyntaxError {
 /// It enforces the following constraints:
 /// - The class of each instance must be at least `1.0`. Classes are floored to integers.
 /// - The number of features in each instance must be consistent.
-pub fn parse_dataset(s: &str) -> Result<Dataset, BadDatasetSyntaxError> {
+pub fn parse_dataset(s: &str) -> Result<Dataset, DatasetSyntaxError> {
     let mut instances = vec![];
     let mut class_count = 0;
     let mut data_set_feature_count = 0;
@@ -32,23 +82,23 @@ pub fn parse_dataset(s: &str) -> Result<Dataset, BadDatasetSyntaxError> {
 
         let mut parts = line.split_whitespace();
 
-        let class = parts.next().ok_or(BadDatasetSyntaxError::new(&format!(
+        let class = parts.next().ok_or(DatasetSyntaxError::new(&format!(
             "missing class on line {line_number}"
         )))?;
         let class: f64 = class
             .parse::<f64>()
             .map_err(|_| {
-                BadDatasetSyntaxError::new(&format!("invalid class on line {line_number}: {class}"))
+                DatasetSyntaxError::new(&format!("invalid class on line {line_number}: {class}"))
             })?
             .floor();
         if class < 1.0 {
-            return Err(BadDatasetSyntaxError::new(&format!(
+            return Err(DatasetSyntaxError::new(&format!(
                 "invalid class on {line_number}: {class}. class must be no less than 1"
             )));
         }
-        let class = class as usize;
+        let class = ClassStartingFrom1(class as usize);
 
-        class_count = class_count.max(class);
+        class_count = class_count.max(class.0);
 
         let instance_feature_count = parts.clone().count();
 
@@ -57,7 +107,7 @@ pub fn parse_dataset(s: &str) -> Result<Dataset, BadDatasetSyntaxError> {
         }
 
         if instance_feature_count != data_set_feature_count {
-            return Err(BadDatasetSyntaxError::new(&format!(
+            return Err(DatasetSyntaxError::new(&format!(
                 "inconsistent feature count on line {line_number}. expected {data_set_feature_count} features, but got {instance_feature_count}"
             )));
         }
@@ -65,12 +115,12 @@ pub fn parse_dataset(s: &str) -> Result<Dataset, BadDatasetSyntaxError> {
         let feature_values = parts
             .map(|part| {
                 part.parse::<f64>().map_err(|_| {
-                    BadDatasetSyntaxError::new(&format!(
+                    DatasetSyntaxError::new(&format!(
                         "invalid feature value on line {line_number}: {part}"
                     ))
                 })
             })
-            .collect::<Result<Vec<f64>, BadDatasetSyntaxError>>()?;
+            .collect::<Result<Vec<f64>, DatasetSyntaxError>>()?;
 
         instances.push(Instance {
             class,
--- project2/src/lib.rs
+++ project2/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod io;
 
+use io::*;
+
 #[derive(Debug)]
 pub struct Dataset {
     pub class_count: usize,
@@ -7,9 +9,75 @@ pub struct Dataset {
     pub instances: Vec<Instance>,
 }
 
+/// We create a new type to help us remember
+/// that the classes are counted starting from 1.
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct ClassStartingFrom1(pub usize);
+
+/// We create a new type to help us remember
+/// that the features are counted starting from 1.
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct FeatureStartingFrom1(pub usize);
+
 #[derive(Debug)]
 pub struct Instance {
     /// Starts from 1.
-    pub class: usize,
+    pub class: ClassStartingFrom1,
     pub feature_values: Vec<f64>,
+}
+
+pub fn forward_search(dataset: &Dataset) -> Vec<FeatureStartingFrom1> {
+    println!("Beginning forward search.");
+
+    let mut current_set = vec![];
+
+    for _ in 0..dataset.feature_count {
+        let mut best_feature: Option<FeatureStartingFrom1> = None;
+        let mut best_accuracy = -1.0;
+
+        for candidate_feature in dataset.features() {
+            // Don't add the feature if it's already in the set.
+            if current_set.contains(&candidate_feature) {
+                continue;
+            }
+
+            let accuracy =
+                leave_out_one_cross_validation(&dataset, &current_set, candidate_feature);
+
+            println!(
+                "    Using feature(s) {} accuracy is {:.1}%",
+                (current_set.as_slice(), candidate_feature).pretty(),
+                accuracy * 100.0
+            );
+
+            if accuracy > best_accuracy {
+                best_accuracy = accuracy;
+                best_feature = Some(candidate_feature);
+            }
+        }
+
+        let best_feature = best_feature.unwrap();
+        current_set.push(best_feature);
+        println!(
+            "Feature set {} was best, accuracy is {:.1}%",
+            current_set.pretty(),
+            best_accuracy * 100.0
+        );
+    }
+
+    current_set
+}
+
+pub fn leave_out_one_cross_validation(
+    dataset: &Dataset,
+    current_set: &[FeatureStartingFrom1],
+    candidate_feature: FeatureStartingFrom1,
+) -> f64 {
+    todo!()
+}
+
+impl Dataset {
+    pub fn features(&self) -> impl Iterator<Item = FeatureStartingFrom1> {
+        (1..=self.feature_count).map(FeatureStartingFrom1)
+    }
 }
